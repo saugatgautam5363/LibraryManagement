a9523adfe710bca00bed0fedf292f84ce401f7ee
saugatgautam5363 <156888983+saugatgautam5363@users.noreply.github.com>
2025-05-06 20:41:17 +0545

    change library system

---
+++ Liabrary/src/Services/FileHandlingServices.java
@@ -0,0 +1,32 @@
+package Services;
+
+import java.io.*;
+
+public class FileHandlingServices {
+    public void WriteLog(String logData){
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter("log.data",true))){
+            bw.write(logData);
+            bw.newLine();
+        }catch (IOException e){
+            System.out.println("Message: "+e.getMessage());
+        }
+    }
+    public void ReadLog(){
+        BufferedReader br = null;
+        try{
+            br = new BufferedReader(new FileReader("log.data"));
+            String line;
+            while((line = br.readLine()) != null){
+                System.out.println(line);
+            }
+        }catch (IOException e){
+            System.out.println("Message: "+e.getMessage());
+        }finally {
+            try {
+                while (br != null) br.close();
+            }catch (IOException e){
+                System.out.println("Close Message: "+e.getMessage());
+            }
+        }
+    }
+}
