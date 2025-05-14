928b5b7a63040de3954255f3d27f6e92c1fcb011
saugatgautam5363 <156888983+saugatgautam5363@users.noreply.github.com>
2025-05-14 21:03:03 +0545

    library System book insert to database

---
+++ Liabrary/src/DataBase/BookInsertDB.java
@@ -0,0 +1,34 @@
+package DataBase;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class BookInsertDB {
+
+    public static void Insert(int id, String bookName, String author, int quantity) {
+        String query = "INSERT INTO books(id, title, author, quantity) VALUES (?, ?, ?, ?)";
+
+        try (Connection connection = DBConnection.getconnection()) {
+            try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+
+                preparedStatement.setInt(1, id);
+                preparedStatement.setString(2, bookName);
+                preparedStatement.setString(3, author);
+                preparedStatement.setInt(4, quantity);
+
+                int row = preparedStatement.executeUpdate();
+                if (row > 0) {
+                    System.out.println("Book Inserted Successfully!!");
+                } else {
+                    System.out.println("Book Not Inserted!!");
+                }
+
+            } catch (SQLException e) {
+                System.out.println("Error preparing or executing statement: " + e.getMessage());
+            }
+        } catch (SQLException e) {
+            System.out.println("Error connecting to database: " + e.getMessage());
+        }
+    }
+}
